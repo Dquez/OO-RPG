54e51ea0bae2309746b75d77515e42bd2b212fab
Dariell Vasquez <dariellv@yahoo.com>
2018-10-18 12:48:52 -0400

    Can manipulate character health, hide defeated characters

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,22 +5,26 @@ $(document).ready(function () {
             Daenerys: {
                 health: 100,
                 attack: 14,
-                counterAttack: 5
+                counterAttack: 5,
+                baseAP: 14
             },
             KhalDrogo: {
                 health: 150,
                 attack: 8,
-                counterAttack: 20
+                counterAttack: 20,
+                baseAP: 8
             },
             Jaime: {
                 health: 120,
-                attack: 8,
-                counterAttack: 15
+                attack: 10,
+                counterAttack: 15,
+                baseAP: 10
             },
             JonSnow: {
                 health: 180,
                 attack: 7,
-                counterAttack: 25
+                counterAttack: 25,
+                baseAP: 7
             }
         },
         characterChosen: null,
@@ -30,7 +34,7 @@ $(document).ready(function () {
 
 
         start: function (name) {
-           
+
             if (this.characterChosen) {
                 return;
             }
@@ -48,57 +52,75 @@ $(document).ready(function () {
                 var characterName = character.id;
                 var stats = $(character).find(".stats");
                 stats.html(
-                    "<p>Health : " + GotCharacters[characterName].health + "</p>" +
-                    "<p>Attack : " + GotCharacters[characterName].attack + "</p>" +
-                    "<p>Counter Attack : " + GotCharacters[characterName].counterAttack + "</p>"
+                    "<p>Health : <span class='health'>" + GotCharacters[characterName].health + "</span></p>" +
+                    "<p>Attack : <span class='attack'>" + GotCharacters[characterName].attack + "</span></p>" +
+                    "<p>Counter Attack : <span class='counter'>" + GotCharacters[characterName].counterAttack + "</span></p>"
                 )
             })
             characters = characters.filter(function (index, character) {
                 return character.id !== name;
             })
-            
+
             $("#enemy-area").append(characters);
         },
         chooseAttacker: function (name) {
             this.gameInPlay = true;
-            var enemy = $("#enemy-area").find("#"+ this.enemyChosen);
-            console.log(enemy);
+            this.enemiesWaiting = this.enemiesWaiting.filter(function (enemy) {
+                return enemy !== this.enemyChosen;
+            })
+            var enemy = $("#enemy-area").find("#" + this.enemyChosen);
             $("#defender-area").append(enemy);
+            $("#enemy-area").parent().css("display", "none");
+
+            // Attack button
             
+            var isButtonAppended = $("#" + this.characterChosen).find(".attackBtn");
+            if (isButtonAppended.length < 1) {
+                var attackBtn = $("<button>Attack</button>");
+                attackBtn.addClass("btn attackBtn btn-danger btn-md");
+                $("#" + this.characterChosen).append(attackBtn);
+            }
         }
 
 
     }
 
 
-$(".card").on("click", function () {
-    GoTRPG.start($(this).attr("id"));
+    $(".card").on("click", function () {
+        GoTRPG.start($(this).attr("id"));
 
-})
+    })
 
-$("#enemy-area").on("click", function(event) {
-    GoTRPG.enemyChosen = event.target.getAttribute("data-name");
+    $("#enemy-area").on("click", function (event) {
+        if (!GoTRPG.enemyChosen) {
+            GoTRPG.enemyChosen = event.target.getAttribute("data-name");
+            GoTRPG.chooseAttacker();
+        }
+    })
 
-    GoTRPG.chooseAttacker();
-})
+    $(document).on("click", ".attackBtn", function () {
+        var enemy = GoTRPG.enemyChosen;
+        var character = GoTRPG.characterChosen;
+        GoTRPG.characters[enemy].health -= GoTRPG.characters[character].attack;
+        GoTRPG.characters[character].attack += GoTRPG.characters[character].baseAP;
+        $(".characters").find(".attack").text(GoTRPG.characters[character].attack);
+        $("#defender-area").find(".health").text(GoTRPG.characters[enemy].health);
+        if (GoTRPG.characters[enemy].health <= 0) {
+            if (GoTRPG.enemiesWaiting.length === 0) {
+                $("#defender-area").empty();
+                alert("You win");
+            } else {
+                $("#defender-area").empty();
+                $("#enemy-area").parent().css("display", "initial");
+                GoTRPG.enemyChosen = false;
+            }
+        } else {
+            GoTRPG.characters[character].health -= GoTRPG.characters[enemy].counterAttack;
+            $(".characters").find(".health").text(GoTRPG.characters[character].health);
+        }
+    })
 
 });
-
-
-
-// When the game starts, the player will choose a character by clicking on the fighter's picture. The player will fight as that character for the rest of the game.
-
-
-// The player must then defeat all of the remaining fighters. Enemies should be moved to a different area of the screen.
-
-
-// The player chooses an opponent by clicking on an enemy's picture.
-
-
-// Once the player selects an opponent, that enemy is moved to a defender area.
-
-
-// The player will now be able to click the attack button.
 
 // Whenever the player clicks attack, their character damages the defender. The opponent will lose HP (health points). These points are displayed at the bottom of the defender's picture.
 // The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their HP. These points are shown at the bottom of the player character's picture.
